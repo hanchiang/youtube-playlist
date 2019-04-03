e8e074d75cd764ada64d73ff6ed64ff83dda32ff
Han Chiang <yaphc@hotmail.com>
2019-04-03 21:28:24 +0800

    Minor fixes

--- App/Containers/SplashScreen/SplashScreen.js
+++ App/Containers/SplashScreen/SplashScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native'
+import { View, Image } from 'react-native'
 
 import styles from './SplashScreenStyle'
 import { Images } from '../../Theme'
