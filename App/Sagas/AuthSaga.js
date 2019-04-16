a5dc2309d67df7d17af5759ab817545f7c6f3a99
Yap Han Chiang <yaphc@hotmail.com>
2019-04-16 21:10:45 +0800

    Rename Auth to AuthService. Check for result.ok for error and success handling (#14)

--- App/Sagas/AuthSaga.js
+++ App/Sagas/AuthSaga.js
@@ -2,7 +2,7 @@ import { put, call } from 'redux-saga/effects'
 
 import AuthActions from '../Stores/Auth/Actions'
 import UserActions from '../Stores/User/Actions'
-import authService from '../Services/Auth'
+import authService from '../Services/AuthService'
 import { Config } from '../Config'
 
 /**
@@ -10,24 +10,32 @@ import { Config } from '../Config'
  */
 export function* authorize() {
   try {
-    const result = yield call(authService.auth)
-    const { data: decodedIdToken } = yield call(authService.validateIdToken, result.idToken)
+    const tokens = yield call(authService.auth)
 
-    if (decodedIdToken.aud === Config.CLIENT_ID) {
-      yield put(AuthActions.authorizeSuccess(result))
-      // Save user info in user reducer
-      yield put(UserActions.setUser({
-        id: decodedIdToken.sub,
-        email: decodedIdToken.email,
-        emailVerified: decodedIdToken.email_verified === 'true',
-        name: decodedIdToken.name,
-        givenName: decodedIdToken.given_name,
-        familyName: decodedIdToken.family_name,
-        picture: decodedIdToken.picture,
-        locale: decodedIdToken.locale
-      }))
+    // verify id token
+    const result = yield call(authService.validateIdToken, tokens.idToken)
+
+    if (result.ok) {
+      const decodedIdToken = result.data
+
+      if (decodedIdToken.aud === Config.CLIENT_ID) {
+        yield put(AuthActions.authorizeSuccess(tokens))
+        // Save user info in user reducer
+        yield put(UserActions.setUser({
+          id: decodedIdToken.sub,
+          email: decodedIdToken.email,
+          emailVerified: decodedIdToken.email_verified === 'true',
+          name: decodedIdToken.name,
+          givenName: decodedIdToken.given_name,
+          familyName: decodedIdToken.family_name,
+          picture: decodedIdToken.picture,
+          locale: decodedIdToken.locale
+        }))
+      } else {
+        yield put(AuthActions.authorizeFailure('aud claim in id token does not matchh app\'s client id'))
+      }
     } else {
-      yield put(AuthActions.authorizeFailure('aud claim in id token does not matchh app\'s client id'))
+      yield put(AuthActions.authorizeFailure(result.data))
     }
   } catch (error) {
     yield put(
@@ -45,14 +53,20 @@ export function* refreshAccessToken(action) {
     // refresh tokens
     const refreshed = yield call(authService.refreshAccesstoken, action.refreshToken)
     // verify id token
-    const { data: decodedIdToken } = yield call(authService.validateIdToken, refreshed.idToken)
+    const result = yield call(authService.validateIdToken, refreshed.idToken)
 
-    if (decodedIdToken.aud === Config.CLIENT_ID) {
-      yield put(AuthActions.refreshAccessTokenSuccess(refreshed))
+    if (result.ok) {
+      const decodedIdToken = result.data
+
+      if (decodedIdToken.aud === Config.CLIENT_ID) {
+        yield put(AuthActions.refreshAccessTokenSuccess(refreshed))
+      } else {
+        yield put(AuthActions.refreshAccessTokenFailure('aud claim in id token does not match app\'s client id'))
+      }
     } else {
-      yield put(AuthActions.refreshAccessTokenFailure('aud claim in id token does not match app\'s client id'))
+      yield put(AuthActions.refreshAccessTokenFailure(result.data))
     }
   } catch (error) {
-    yield put(AuthActions.refreshAccessTokenFailure(error.message))
+    yield put(AuthActions.refreshAccessTokenFailure(error.message || error.code))
   }
 }
